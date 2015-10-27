e80dd43da5646a76e1ed32eccec2a833219abf06
Stella Ding <yuweid@uw.edu>
2015-10-27 10:09:06 -0700

    hi

--- js/main.js
+++ js/main.js
@@ -2,3 +2,54 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+/*change the content when clicking on the button */
+
+$('#change-content').click(function(){
+
+	var useSelector = $("#selector").val();
+	var changedContent = $("#newContent").val();
+	$('main ' + useSelector).html(changedContent);
+
+
+})
+
+$('#add-at-end').click(function(){
+
+	var useSelector = $("#selector").val();
+	var appendElement = $("#newContent").val();
+	$(useSelector).append(appendElement);
+
+}) 
+
+$('#add-at-start').click(function(){
+
+	var useSelector = $("#selector").val();
+	var appendElement = $("#newContent").val();
+	$(useSelector).prepend(appendElement);
+
+}) 
+
+
+$('#insert-before').click(function(){
+
+	var appendElement = $("#newContent").val();
+	$(commonSelector).before(appendElement);
+
+}) 
+
+$('#move-after').click(function(){
+
+	var useSelector = $("#newContent").val();
+	$(useSelector).after(commonSelector);
+
+}) 
+
+$('#surround-class').click(function(){
+	var useSelector = $("#selector").val();
+	$().wrap( "<div class='new'></div>" );
+
+}) 
+
+
+
+
